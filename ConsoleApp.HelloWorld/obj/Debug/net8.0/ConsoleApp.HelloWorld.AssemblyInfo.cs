da94c7be3023df29f506bc17315bbdb603908743
Tona023 <uniquelylatona23@gmail.com>
2024-03-28 19:28:51 -0500

    Added additional print message

--- ConsoleApp.HelloWorld/obj/Debug/net8.0/ConsoleApp.HelloWorld.AssemblyInfo.cs
+++ ConsoleApp.HelloWorld/obj/Debug/net8.0/ConsoleApp.HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp.HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+898e03743443dfc1fa660a5b2d742699b7a8ef78")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp.HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp.HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
